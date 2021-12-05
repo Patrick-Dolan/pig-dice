5061bb52b2417d4866106fcb51b030b994b5d8cf
Patrick Dolan <dolanp1992@gmail.com>
2021-12-05 11:25:25 -0800

    add Player and Game constructors and global variables as a placeholder for database
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ console.log("Scripts Loaded");
 function rollDice() {
   return Math.floor((Math.random() * 6 ) + 1);
 }
+// Player Business Logic
+function Player(name) {
+  this.name = name;
+  this.score = 0;
+  this.turns = 0;
+}
+// Game Business Logic
+function Game() {
+  this.players = [player1, player2];
+}
+
+// Global Variables
+const player1 = new Player("Branden");
+const player2 = new Player("Patrick");
+const game = new Game();
