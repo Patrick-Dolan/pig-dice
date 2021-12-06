f03dad054c5e4f6a0c8206c3111a150522038570
Patrick Dolan <dolanp1992@gmail.com>
2021-12-05 16:17:22 -0800

    change some indentation errors
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -23,28 +23,27 @@ Game.prototype.checkRoll = function() {
   } else {
     console.log("rolled a 1");
     this.curPlayer.turns[this.curPlayer.turns.length - 1].push(rollValue);
-    //this.turnChange();
   }
 };
 
 Game.prototype.turnChange = function() {
-    let nextTurn = Infinity;
-    let currentPlayer;
-    this.players.forEach(function(player) {
-      if (player.turns.length < nextTurn) {
-        currentPlayer = player;
-        nextTurn = player.turns.length;
-      }
-    });
-    this.curPlayer = currentPlayer;
-    this.curPlayer.turns.push([])
+  let nextTurn = Infinity;
+  let currentPlayer;
+  this.players.forEach(function(player) {
+    if (player.turns.length < nextTurn) {
+      currentPlayer = player;
+      nextTurn = player.turns.length;
+    }
+  });
+  this.curPlayer = currentPlayer;
+  this.curPlayer.turns.push([])
 };
 
 Game.prototype.scoreTotal = function() {
   let scoreTotal = 0;
   for (let i = 0; i < this.curPlayer.turns.length; i++) {
     if (this.curPlayer.turns[i].includes(1)) {
-      console.log("Includes a 1")
+      console.log("Includes a 1");
     } else {
       scoreTotal += this.curPlayer.turns[i].reduce((a, b) => a + b, 0);
     }
