9a2c880ecc397d3f3b727366d1d94422fc0218d4
Patrick Dolan <dolanp1992@gmail.com>
2021-12-05 16:06:45 -0800

    add scoreTotal prototype to tabulate scores
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,20 @@ function Player(name) {
 }
 // Game Business Logic
 function Game() {
-  this.players = [player1, player2];
-  //this.curPlayer = [];
+  this.players = [player1, player2, player3];
 }
 
 Game.prototype.checkRoll = function() {
   let rollValue = rollDice();
   if (rollValue > 1) {
     this.curPlayer.turns[this.curPlayer.turns.length - 1].push(rollValue);
+    console.log(this.curPlayer.turns);
   } else {
     console.log("rolled a 1");
+    this.curPlayer.turns[this.curPlayer.turns.length - 1].push(rollValue);
+    //this.turnChange();
   }
-}
+};
 
 Game.prototype.turnChange = function() {
     let nextTurn = Infinity;
@@ -38,9 +40,20 @@ Game.prototype.turnChange = function() {
     this.curPlayer.turns.push([])
 };
 
-
+Game.prototype.scoreTotal = function() {
+  let scoreTotal = 0;
+  for (let i = 0; i < this.curPlayer.turns.length; i++) {
+    if (this.curPlayer.turns[i].includes(1)) {
+      console.log("Includes a 1")
+    } else {
+      scoreTotal += this.curPlayer.turns[i].reduce((a, b) => a + b, 0);
+    }
+  }
+  this.curPlayer.score = scoreTotal;
+};
 
 // Global Variables
 const player1 = new Player("Branden");
 const player2 = new Player("Patrick");
+const player3 = new Player("Jack");
 const game = new Game();
